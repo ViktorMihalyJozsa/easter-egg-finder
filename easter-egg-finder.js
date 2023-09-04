78d632a7caab1fbd2c7c204a3de6d1f65cacb725
VMJ <viktor.mihaly.jozsa@gmail.com>
2023-09-04 21:22:01 +0200

    responsive canvas width height

--- easter-egg-finder.js
+++ easter-egg-finder.js
@@ -9,13 +9,13 @@ const bodyHeight = document.body.clientHeight; // a body magassága
 const canvas = document.getElementById('myCanvas'); // a canvas elem
 const c = canvas.getContext('2d'); // a canvas contextje
 
-size = (bodyWidth * 0.6) / 20; // a mezők mérete
+const size = 45;
 
-canvas.width = size * 20; // a canvas szélessége
-canvas.height = size * 10; // a canvas magassága
+canvas.width = (bodyWidth * 0.6) - ((bodyWidth * 0.6) % size); // a canvas szélessége
+canvas.height = (bodyHeight * 0.6) - ((bodyHeight * 0.6) % size); // a canvas magassága
 
-const columns = 20; // oszlopok száma
-const rows = 10; // sorok száma
+const columns = canvas.width / size;
+const rows = canvas.height / size;
 
 // ----------  E N D   C A N V A S  -  S T Y L E  ------------------------------ //
 // ----------------------------------------------------------------------------- //
