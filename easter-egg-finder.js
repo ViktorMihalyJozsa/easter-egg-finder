e08cd443511a43f5aaf927ff4b55c1e2b22174a5
VMJ <viktor.mihaly.jozsa@gmail.com>
2023-09-04 22:30:58 +0200

    mineCount = 10

--- easter-egg-finder.js
+++ easter-egg-finder.js
@@ -25,7 +25,7 @@ const mineCounter = document.getElementById('mine-count');
 const timeCounter = document.getElementById('time');
 
 const mine = 'mine';
-const mineCount = 5;
+const mineCount = 10;
 const images = {
   'hidden': document.getElementById('hidden'),
   'mine': document.getElementById('exploded-mine'),
