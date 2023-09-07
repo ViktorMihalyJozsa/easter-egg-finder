50ba0b1cda1ee9ca05fa7789098e342755ce48bf
VMJ <viktor.mihaly.jozsa@gmail.com>
2023-09-07 19:12:09 +0200

    html class myCanvas

--- easter-egg-finder.js
+++ easter-egg-finder.js
@@ -27,7 +27,7 @@ const mineCounter = document.getElementById('mine-count');
 const timeCounter = document.getElementById('time');
 
 const mine = 'mine';
-const mineCount = 20;
+const mineCount = 5;
 const images = {
   'hidden': document.getElementById('hidden'),
   'mine': document.getElementById('exploded-mine'),
