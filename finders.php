813669ea8b0ab35bbfeda53ce467d831e065d6f4
VMJ <viktor.mihaly.jozsa@gmail.com>
2024-10-11 20:56:33 +0200

    PHP - Menu

---
+++ finders.php
@@ -0,0 +1,22 @@
+<div class="myfinders">
+    <img class="myhome-top-right" src="assets/easter-like-rabbit.gif" alt="Easter Like Rabbit">
+    <img class="mycontact-bottom-left" src="assets/basket-easter-eggs.png" alt="Basket Easter Egg">
+    <img class="mycontact-bottom-right" src="assets/easter-grass.png" alt="Easter Grass">
+    <div class="myfinders-top-left">
+        <ul>
+            <li>
+        <a href="https://viktormihalyjozsa.github.io/course-js-minesweeper/" 
+           title="Minesweeper" class="finders-icon" target="_blank">~ MINESWEEPER</a>
+        <p class="center">( Sorry , it only works on monitor . )</p>
+            </li>
+            <li>
+        <a href="https://viktormihalyjozsa.github.io/valentin-s-day-heart-finder/" 
+           title="Valentine's Day" class="finders-icon" target="_blank">~ VALENTIN'S DAY</a>
+            </li>
+            <li>
+        <a href="https://viktormihalyjozsa.github.io/easter-egg-finder/" 
+           title="Easter Holiday's"  class="finders-icon" target="_blank">~ EASTER HOLIDAY'S</a>
+            </li>
+        </ul>
+    </div>
+</div>
